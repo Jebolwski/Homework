3c86b938ef43e5973f8b6e8f5407b0344bcd118c
Mert Gökmen <mertgkmeen@gmail.com>
2022-01-19 13:25:05 +0300

    dam

--- selectionsort.py
+++ selectionsort.py
@@ -21,14 +21,54 @@
 #             dizi[i]=dizi[j]
 # print(dizi)
 
-string = "RasimÖzhan"
-dizi= []
-tersdizi = []
-for i in string:
-    dizi.append(i)
+# string = "RasimÖzhan"
+# dizi= []
+# tersdizi = []
+# for i in string:
+#     dizi.append(i)
 
-for i in range(len(dizi)):
-    a=dizi.pop()
-    tersdizi.append(a)
-print(tersdizi)
+# for i in range(len(dizi)):
+#     a=dizi.pop()
+#     tersdizi.append(a)
+# print(tersdizi)
+
+# array = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+# for i in range(4,len(array)-1):
+#     array[i]=array[i+1]
+#     array[4]=24
+# print(array)
+
+
+# array = []
+
+# array.append(1)
+# print(array)
+# array.append(2)
+# print(array)
+# array.append(3)
+# print(array)
+# array.append(4)
+# print(array)
+# array.append(5)
+# print(array)
+# array.pop()
+# print(array)
+# array.pop()
+# print(array)
+# array.pop()
+# print(array)
+# array.pop()
+# print(array)
+
+def faktor(b):
+    if b==0 or b==1:
+        return 1
+    else:
+        return b*faktor(b-1)
+a=int(input("Kaçın faktöriyeli:"))
+res=faktor(a)
+print(res)
+
+
 
